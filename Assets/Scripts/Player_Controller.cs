1a3491f74eb963c9646895b74a99bd1c72a03656
Никита <Promaren2017@yandex.ru>
2022-05-06 18:23:46 +0400

    Готово второе задание

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
+
+public class Player_Controller : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _Joystick;
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private float _move_Spead;
+
+    private void FixedUpdate()
+    {
+        _rigidbody.velocity = new Vector3(_Joystick.Horizontal * _move_Spead, _rigidbody.velocity.y, _Joystick.Vertical * _move_Spead);
+
+        if (_Joystick.Horizontal != 0 || _Joystick.Vertical != 0)
+        {
+            transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);  
+        }
+    }
+}
